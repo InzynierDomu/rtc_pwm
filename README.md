059234ecf610e9d78a2a34f9c71d386cd1cf1b91
Inżynier Domu <inzynier.domu@gmail.com>
2021-11-23 23:56:48 +0100

    sunrise and sunset

--- README.md
+++ README.md
@@ -21,4 +21,8 @@ const uint8_t m_pwm_resolution = 255; ///< finish duty cycle pwm for sunrise
 ## Scheme
 
 ## IDE
-The project is prepared for the Platform IO environment. A video on how to install such an environment can be watched on this [video](https://youtu.be/Em9NuebT2Kc)
+The project is prepared for the Platform IO environment. A video on how to install such an environment can be watched on this [video](https://youtu.be/Em9NuebT2Kc).
+<br><br>
+Formatting is done using clang-format. The description of the tool configuration is in the [video](https://youtu.be/xxuaOG0WjIE).
+<br><br>
+The code contains a comment prepared for doxygen, their use is described in the [video](https://youtu.be/1YKJtrCsPD4).
--- src/main.cpp
+++ src/main.cpp
@@ -6,19 +6,30 @@
  * @date 07-2021
  */
 
+#include "RTClib.h"
+
 #include <Arduino.h>
 #include <Wire.h>
-#include "RTClib.h"
 
 RTC_DS1307 m_rtc; ///< DS1307 RTC
 
-const byte  m_pwm_pin = D8;   ///< pin for led
-const DateTime m_start_time = DateTime(2000, 1, 1, 8, 10, 0); ///< time when surise start
-const uint8_t m_sunrise_time = 30;    ///< duration of sunrise
-const uint8_t m_min_in_h = 60;        ///< minutes in an hour
-const unsigned long m_refresh_time_ms = 15000;  ///< time of repeting check time is in range and sending message
-const uint8_t m_starting_pwm = 0;     ///< starting duty cycle pwm for sunrise
+const byte m_pwm_pin = D8; ///< pin for led
+const uint8_t m_starting_pwm = 0; ///< starting duty cycle pwm for sunrise
 const uint8_t m_pwm_resolution = 255; ///< finish duty cycle pwm for sunrise
+
+const DateTime m_sunrise_time = DateTime(2000, 1, 1, 8, 10, 0); ///< time when surise start
+const DateTime m_sunset_time = DateTime(2000, 1, 1, 20, 10, 0); ///< time when sunset start
+const uint8_t m_duration = 30; ///< duration of sunrise or sunset
+const uint8_t m_min_in_h = 60; ///< minutes in an hour
+const unsigned long m_refresh_time_ms = 5000; ///< time of repeting check time is in range and sending message
+
+enum class day_state
+{
+  NIGHT,
+  SUNRISE,
+  DAY,
+  SUNSET
+};
 
 /**
  * @brief calculate hours and minutes to only minutes
@@ -38,7 +49,18 @@ uint16_t min_calculate(const DateTime& time)
  */
 void sunrise(const int time_in_min)
 {
-  auto pwm = map(time_in_min, 0, m_sunrise_time, m_starting_pwm, m_pwm_resolution);
+  auto pwm = map(time_in_min, 0, m_duration, m_starting_pwm, m_pwm_resolution);
+  analogWrite(m_pwm_pin, pwm);
+  Serial.println(pwm);
+}
+
+/**
+ * @brief changing duty cycle pwm output proportionally to sunset progress
+ * @param time_in_min time from sunset start
+ */
+void sunset(const int time_in_min)
+{
+  auto pwm = map(time_in_min, 0, m_duration, m_pwm_resolution, m_starting_pwm);
   analogWrite(m_pwm_pin, pwm);
   Serial.println(pwm);
 }
@@ -46,33 +68,46 @@ void sunrise(const int time_in_min)
 /**
  * @brief setup
  */
-void setup() 
+void setup()
 {
   Wire.begin();
-  Serial.begin(9600);  
+  Serial.begin(9600);
 
-  if (!m_rtc.begin()) 
+  if (!m_rtc.begin())
   {
     Serial.println("Couldn't find RTC");
   }
 
+#ifdef DEBUG
   if (!m_rtc.isrunning())
   {
-    Serial.println("RTC is NOT running, let's set the time!");    
+    Serial.println("RTC is NOT running");
   }
+#endif
 
   m_rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-  analogWrite(m_pwm_pin, 0);
+
+  DateTime now = m_rtc.now();
+  if ((min_calculate(now) >= min_calculate(m_sunrise_time)) && (min_calculate(now) <= min_calculate(m_sunset_time)))
+  {
+    analogWrite(m_pwm_pin, m_pwm_resolution);
+  }
+  else
+  {
+    analogWrite(m_pwm_pin, m_starting_pwm);
+  }
 }
 
 /**
  * @brief main loop
  */
-void loop() 
-{  
+void loop()
+{
   static unsigned long last_loop_time = 0;
   unsigned long loop_time = millis();
-  if(loop_time - last_loop_time > m_refresh_time_ms)
+
+
+  if (loop_time - last_loop_time > m_refresh_time_ms)
   {
     DateTime now = m_rtc.now();
     Serial.print(now.hour(), DEC);
@@ -81,10 +116,14 @@ void loop()
     Serial.print(":");
     Serial.println(now.second(), DEC);
     last_loop_time = millis();
-    auto sunrise_time = min_calculate(now) - min_calculate(m_start_time);
-    if((sunrise_time >= 0) && (sunrise_time <= m_sunrise_time ))
+
+    if ((min_calculate(now) >= min_calculate(m_sunrise_time)) && (min_calculate(now) + m_duration <= min_calculate(m_sunrise_time)))
     {
-      sunrise(sunrise_time);
+      sunrise(min_calculate(now) - min_calculate(m_sunrise_time));
+    }
+    else if ((min_calculate(now) >= min_calculate(m_sunset_time)) && (min_calculate(now) + m_duration <= min_calculate(m_sunset_time)))
+    {
+      sunset(min_calculate(now) - min_calculate(m_sunset_time));
     }
   }
 }
